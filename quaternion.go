d8fbf5b9171b548f4e6ea1f7a300ef3a0913f638
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-07-01 10:39:21 +0200

    Added Invert(), RotateX(), RotateY(), RotateZ()

--- quaternion.go
+++ quaternion.go
@@ -36,6 +36,78 @@ func (quat *Quaternion[T]) Identity() {
 	quat[3] = 1
 }
 
+func (quat *Quaternion[T]) Invert() {
+	a0 := quat[0]
+	a1 := quat[1]
+	a2 := quat[2]
+	a3 := quat[3]
+
+	dot := a0*a0 + a1*a1 + a2*a2 + a3*a3
+
+	var invDot T
+	if dot == 0 {
+		quat[0] = 0
+		quat[1] = 0
+		quat[2] = 0
+		quat[3] = 0
+	} else {
+		invDot = 1.0 / dot
+
+		quat[0] = -a0 * invDot
+		quat[1] = -a1 * invDot
+		quat[2] = -a2 * invDot
+		quat[3] = a3 * invDot
+	}
+}
+
+func (quat *Quaternion[T]) RotateX(angle float64) {
+	angle *= 0.5
+
+	ax := float64(quat[0])
+	ay := float64(quat[1])
+	az := float64(quat[2])
+	aw := float64(quat[3])
+	bx := math.Sin(angle)
+	bw := math.Cos(angle)
+
+	quat[0] = T(ax*bw + aw*bx)
+	quat[1] = T(ay*bw + az*bx)
+	quat[2] = T(az*bw - ay*bx)
+	quat[3] = T(aw*bw - ax*bx)
+}
+
+func (quat *Quaternion[T]) RotateY(angle float64) {
+	angle *= 0.5
+
+	ax := float64(quat[0])
+	ay := float64(quat[1])
+	az := float64(quat[2])
+	aw := float64(quat[3])
+	by := math.Sin(angle)
+	bw := math.Cos(angle)
+
+	quat[0] = T(ax*bw - az*by)
+	quat[1] = T(ay*bw + aw*by)
+	quat[2] = T(az*bw + ax*by)
+	quat[3] = T(aw*bw - ay*by)
+}
+
+func (quat *Quaternion[T]) RotateZ(angle float64) {
+	angle *= 0.5
+
+	ax := float64(quat[0])
+	ay := float64(quat[1])
+	az := float64(quat[2])
+	aw := float64(quat[3])
+	bz := math.Sin(angle)
+	bw := math.Cos(angle)
+
+	quat[0] = T(ax*bw + ay*bz)
+	quat[1] = T(ay*bw - ax*bz)
+	quat[2] = T(az*bw + aw*bz)
+	quat[3] = T(aw*bw - az*bz)
+}
+
 func (quat *Quaternion[T]) String() string {
 	return fmt.Sprintf("[%f, %f, %f, %f]", float32(quat[0]), float32(quat[1]), float32(quat[2]), float32(quat[3]))
 }
